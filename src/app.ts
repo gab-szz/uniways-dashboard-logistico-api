c74d1b653dd5b46d207be8559c87da27dbe3a1da
gab-szz <silvio.apad@gmail.com>
2026-04-29 15:09:12 -0400

    feat(cors): adicionar suporte ao CORS e ajustar limite de requisições

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import rateLimit from '@fastify/rate-limit';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { guardMiddleware } from './middlewares/guard.middleware.js';
@@ -8,13 +9,19 @@ import { swagger } from './config/swagger.js';
 
 const servidor = fastify();
 
+await servidor.register(cors, {
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+});
+
 servidor.setValidatorCompiler(validatorCompiler);
 servidor.setSerializerCompiler(serializerCompiler);
 
 swagger(servidor);
 
 servidor.register(rateLimit, {
-  max: 30,
+  max: 60,
   timeWindow: '1 minute',
   allowList: [],
   keyGenerator: (req: any) => req.ip,
